69145cf97d47c392a67087488e251242d46bfa8d
Thiago Rodrigues <rodriguesthiago@brq.com>
2020-07-02 19:01:52 -0300

    novos beans para a mensageria

---
+++ src/main/java/br/com/climb/commons/execptions/NotConnectionException.java
@@ -0,0 +1,7 @@
+package br.com.climb.commons.execptions;
+
+public class NotConnectionException extends Exception {
+    public NotConnectionException(String message) {
+        super(message);
+    }
+}
